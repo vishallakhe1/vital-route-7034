6066c93dde2f639c17f84191bc9f3b6b14a0f9d6
vishallakhe1 <vishallakhe1@gmail.com>
2023-07-20 23:04:50 +0530

    react_Proj

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import Navbar from './Routes/Navbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar/>
     </div>
   );
 }
--- my-app/src/Routes/Navbar.jsx
+++ my-app/src/Routes/Navbar.jsx
@@ -1,10 +1,10 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import logo from "../../src/Logo.png";
+import logo from "../../src/Images/RCT_Proj_Logo.png";
 import { Link as ScrollLink, animateScroll as scroll } from "react-scroll";
 import { Button } from "@chakra-ui/react";
-// import { AuthContext } from "../Context/AuthContextProvider"
-import AuthContextProvider from "../Context/AuthContextProvider";
+import { AuthContext } from "../Context/AuthContextProvider"
+
 
 const Navbar = () => {
 
@@ -16,9 +16,81 @@ const Navbar = () => {
         { to: "home-news-sec", text: "News" },
         { to: "/Products", text: "Products" },
         { to: "/ShoppingCart", text: "ShoppingCart" },
-        { to: "/Login", text: "Login" },
-        { to: "/SignUp", text: "SignUp" }
       ];
+
+
+      const scrollToTop = () => {
+        scroll.scrollToTop();
+      };
+    
+      const handleLogout = () => {
+        setIsAuth(false); // Update the authentication state to false on logout
+      };
+    
+      return (
+        <div className="navbar">
+          <div>
+            <img src={logo} alt="Logo" className="navbar-logo" />
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                width: "60%",
+              }}
+            >
+              {links.map((link, index) => {
+                if (link.to === "home-about-sec") {
+                  return (
+                    <ScrollLink
+                      key={index}
+                      to={link.to}
+                      smooth={true}
+                      duration={500}
+                      className="navbar-link"
+                    >
+                      {link.text}
+                    </ScrollLink>
+                  );
+                } else if (link.to === "home-news-sec") {
+                  return (
+                    <ScrollLink
+                      key={index}
+                      to={link.to}
+                      smooth={true}
+                      duration={500}
+                      className="navbar-link"
+                      onClick={scrollToTop}
+                    >
+                      {link.text}
+                    </ScrollLink>
+                  );
+                } else {
+                  return (
+                    <Link key={index} to={link.to} className="navbar-link">
+                      {link.text}
+                    </Link>
+                  );
+                }
+              })}
+              {isAuth ? (
+                <Button colorScheme="telegram" onClick={handleLogout}>
+                  Logout
+                </Button>
+              ) : (
+                <>
+                  <Link to="/login" className="navbar-link">
+                    Login
+                  </Link>
+                  <Link to="/signup" className="navbar-link">
+                    Signup
+                  </Link>
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+      );
 
 }
 
